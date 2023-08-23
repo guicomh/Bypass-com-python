8dd7068f29e54d3067cf3280d402889e11f4478a
Guilherme <eliseuvasconcellos@gmail.com>
2023-08-23 15:13:20 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,21 +1,18 @@
 import os 
 
-os.system("Title Bypass.gui")
+os.system("Title Bypassgui")
 
 temp = input("Apagar arquivos do seu computador? (y/n) >")
 
 if temp == 'y':
 
-# apagando arquivos temporarios e recents 
+# apagando arquivos temporarios
     os.system("rd /q %temp%")
     os.system("RD/s/q Recent")
 
-# apagando a pasta mods fivem
-    os.system("cd C:\Users\elise\AppData\Local\FiveM\FiveM.app")
+# apagando pasta mods fivem
+    os.system("cd AppData\Local\FiveM\FiveM.app")
     os.system("RD/s/q mods")
-
-    
-
 
 if temp == 'n':
 
@@ -23,7 +20,7 @@ if temp == 'n':
     os.system("pause")   
 
 
-os.system("pause");
+os.system("pause")
 
 
   
