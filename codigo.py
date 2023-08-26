d0b85fcb942c7839626cc338bd2c11b747a5f21b
Guilherme <eliseuvasconcellos@gmail.com>
2023-08-26 04:10:58 -0300

    icone

--- codigo.py
+++ codigo.py
@@ -6,11 +6,20 @@ def apagar_tudo():
 
     os.system("rd/s/q %temp%")
     os.system("RD/s/q Recent")
-    os.system("cd AppData\Local\FiveM\FiveM.app")
-    os.system("rd/s/q mods")     
+    os.system("cd .. && cd .. & cd .. & cd .. &&cd AppData\Local\FiveM\FiveM.app && rd/s/q mods")
+    os.system("cd .. && cd .. & cd .. & cd .. &&cd AppData\Local\FiveM\FiveM.app && rd/s/q logs")  
+
+def fivem_a():
+
+    os.system("cd .. && cd .. & cd .. & cd .. && cd AppData\Local\FiveM && start fivem")
+    
+
+ 
     
 
 janela = Tk()
+
+janela.iconbitmap("icone.ico")
 
 janela.title("Bypass do Gui")
 janela.geometry("300x200")
@@ -21,8 +30,11 @@ texto.grid(column=0, row=0, padx=130, pady=1)
 botao = Button(janela, text="Apagar", command=apagar_tudo)
 botao.grid(column=0, row=1, padx=10, pady=10)
 
+botao_limpeza = Button(janela, text='FiveM', command=fivem_a)
+botao_limpeza.grid(column=0 , row=2, padx=15, pady=15)
+
 botao_kendra = Button(janela, text='Fechar', command=janela.destroy)
-botao_kendra.grid(column=0, row=2, padx=20, pady=20)
+botao_kendra.grid(column=0, row=3, padx=20, pady=20)
 
 
 janela.mainloop()
