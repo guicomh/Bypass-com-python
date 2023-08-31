c22eeeb675aa968b53e75574beb648434b11e65f
Guilherme <eliseuvasconcellos@gmail.com>
2023-08-30 21:20:35 -0300

    new

--- codigo.py
+++ codigo.py
@@ -7,11 +7,11 @@ def apagar_tudo():
     os.system("rd/s/q %temp%")
     os.system("RD/s/q Recent")
     os.system("cd .. && cd .. && cd ..  &&cd AppData\Local\FiveM\FiveM.app && rd/s/q mods")
-    os.system("cd .. && cd .. && cd ..  &&cd AppData\Local\FiveM\FiveM.app && rd/s/q logs")  
+    os.system("cd .. && cd .. && cd ..  &&cd AppData\Local\FiveM\FiveM.app && rd/s/q logs ")  
 
 def fivem_a():
 
-    os.system("cd .. && cd .. &&cd ..  &&cd AppData\Local\FiveM && start fivem")
+    os.system("cd .. && cd .. &&cd ..  &&cd AppData\Local\FiveM\FiveM.app\data  && rd/s/q cache && rd/s/q nui-storage && rd/s/q server-cache &&  rd/s/q server-cache-priv ")
     
 
 janela = Tk()
@@ -21,13 +21,13 @@ janela.iconbitmap("icone.ico")
 janela.title("Bypass do Gui")
 janela.geometry("300x200")
 
-texto = Label(janela, text="Bypass")
+texto = Label(janela, text="Limpar Fivem")
 texto.grid(column=0, row=0, padx=130, pady=1)
 
 botao = Button(janela, text="Apagar", command=apagar_tudo)
 botao.grid(column=0, row=1, padx=10, pady=10)
 
-botao_limpeza = Button(janela, text='FiveM', command=fivem_a)
+botao_limpeza = Button(janela, text='Apagar Cache', command=fivem_a)
 botao_limpeza.grid(column=0 , row=2, padx=15, pady=15)
 
 botao_kendra = Button(janela, text='Fechar', command=janela.destroy)
